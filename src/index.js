7ca4952b72a5154e1bca08ecd234ccbbcb7a771b
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-04-30 12:27:21 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import helmet from  'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from "./database/database";
+
+const app = express();
+dotenv.config();
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/', (req, res) => res.status(200).json({message: 'AF Server up and Running'}));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`AF Server running on port ${PORT}`));
