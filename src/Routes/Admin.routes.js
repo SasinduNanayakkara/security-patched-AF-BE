9412d79b0411e51c01737d4904aa40692391a44e
ShavidiniEkanayake <it20631192@my.sliit.lk>
2023-05-07 10:23:21 +0530

    Added api for add admin

--- src/Routes/Admin.routes.js
+++ src/Routes/Admin.routes.js
@@ -4,9 +4,6 @@ import * as AdminController from "../Controllers/Admin.controller";
 const router = Router();
 
 router.post("/", AdminController.addAdmin);
-router.get("/", AdminController.getAdmins);
-router.get("/:id", AdminController.getOneAdmin);
-router.put("/:id", AdminController.updateAdmin);
-router.delete("/:id", AdminController.deleteAdmin);
+
 
 export default router;
