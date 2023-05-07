7e6c334ca102841e12bfa71b247a03e45c6e92bf
shavidiniatsi <shavidiniatsi@gmail.com>
2023-05-07 09:48:37 +0530

    Added routes for add admin

---
+++ src/Routes/Admin.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import * as AdminController from "../Controllers/Admin.controller";
+
+const router = Router();
+
+router.post("/", AdminController.addAdmin);
+
+export default router;
