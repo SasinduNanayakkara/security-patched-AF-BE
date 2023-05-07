009b19ed372f129338ab7252a050bdc0bfc6b752
shavidiniatsi <shavidiniatsi@gmail.com>
2023-05-07 09:53:08 +0530

    Completed add admin API

---
+++ src/Repository/Admin.repository.js
@@ -0,0 +1,11 @@
+import { Admin } from "../Models/Admin.model";
+
+export const addAdmin = async ({firstName, lastName, email, password}) => {
+    try {
+        const result = await Admin.create({firstName, lastName, email, password});
+        return result;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+};
