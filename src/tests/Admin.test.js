7529c0d6eca1293ad7c743e982ceab1be99a61fc
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-05-26 20:48:01 +0530

    Write test case

--- src/tests/Admin.test.js
+++ src/tests/Admin.test.js
@@ -1,42 +1,66 @@
 const request = require("supertest")
 const baseURL = "http://localhost:5000";
 
-// describe(`GET ${baseURL}/api/admin`, () => {
-//     it("should return 200 OK", () => {
-//         const response = request.get("/api/admin");
-//         expect(response.status).toBe(200);
-//         expect(response.body.length).toBeGreaterThan(0);
-//     });
-// });
+describe(`GET ${baseURL}/api/admin`, () => {
+    const newAdmin = {
+                firstName: "John",
+                lastName: "Doe",
+                email: "johndoe@gmail.com",
+                password: "123456",
+    }
+    beforeAll(async () => {
+        await request(baseURL).post("/api/admin").send(newAdmin);
+    });
+    afterAll(async () => {
+        await request(baseURL).delete(`/api/admin/${newAdmin._id}`);
+    });
+    it("should return 200 OK", async () => {
+        const response = await request(baseURL).get("/api/admin");
+        expect(response.statusCode).toBe(200);
+    });
+    it("should return data", async () => {
+        const response = await request(baseURL).get(`/api/admin`);
+        expect(response.body.length >= 1).toBe(true);
+    });
+});
 
-let adminData;
+describe(`POST ${baseURL}/api/admin`, () => {
+    const newAdmin = {
+                firstName: "John",
+                lastName: "Doe",
+                email: "john@gmail.com",
+                password: "123456",
+            }
+    afterAll(async () => {
+        await request(baseURL).delete(`/api/admin/${newAdmin._id}`);
+    });
+    it("should add Admin", async () => {
+        const response = await request(baseURL).post("/api/admin").send(newAdmin);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.data.firstName).toBe(newAdmin.firstName);
+        expect(response.body.data.lastName).toBe(newAdmin.lastName);
+        expect(response.body.data.email).toBe(newAdmin.email);
+    })
 
-describe(`Admin`, () => {
+});
+
+describe(`DELETE ${baseURL}/api/admin`, () => {
     const newAdmin = {
         firstName: "John",
         lastName: "Doe",
-        email: "johndoe@gmail.com",
+        email: "john@gmail.com",
         password: "123456",
     }
-
     beforeAll(async () => {
-        adminData = await request(baseURL)
-        .post("/api/admin")
-        .send(newAdmin);
+        await request(baseURL).post("/api/admin").send(newAdmin);
     });
-    it ("should return 200 OK", async () => {
-        const response = await request(baseURL).get("/api/admin");
-        expect(response.status).toBeEqual(200);
-        expect(response.body.length).toBeGreaterThan(0);
-        expect(response.body.error).not.toBe(null);
-    });
-    it ("should return data", async () => {
-        const response = await request(baseURL).get(`${baseURL}/api/admin/${adminData.body.data._id}`);
-        expect(response.status).toBeEqual(200);
-        expect(response.body.data).not.toBe(null);
-    });
-    // afterAll(async () => {
-    //     await request(baseURL)
-    //     .delete(`/api/admin/${adminData.body.data._id}`);
-    // });
-})
+    it("should delete Admin", async () => {
+        const testRequest = await request(baseURL).get(`/api/admin`);
+        const testAdmin = testRequest.body[testRequest.body.length - 1];
+        const response = await request(baseURL).delete(`/api/admin/${testAdmin._id}`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.firstName).toBe(newAdmin.firstName);
+        expect(response.body.lastName).toBe(newAdmin.lastName);
+        expect(response.body.email).toBe(newAdmin.email);
+    })
+});
