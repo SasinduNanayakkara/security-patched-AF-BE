4732df0b1bbdd3497cfa5798e0e5e5aaccbe7352
Ravindu-Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2023-10-05 21:13:09 +0530

    Client API authentication implemented

--- src/Routes/Client.routes.js
+++ src/Routes/Client.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as ClientController from "../Controllers/Client.controller";
+import {verifyToken} from "../Utils/verifyToken";
 
 const router = Router();
 
-router.post("/", ClientController.createClient);
-router.get("/", ClientController.getALLClients);
-router.get("/:id", ClientController.getOneClientById);
-router.put("/:id", ClientController.updateClientById);
-router.delete("/:id", ClientController.deleteClientById);
+router.post("/", verifyToken,ClientController.createClient);
+router.get("/", verifyToken,ClientController.getALLClients);
+router.get("/:id",verifyToken, ClientController.getOneClientById);
+router.put("/:id",verifyToken, ClientController.updateClientById);
+router.delete("/:id",verifyToken, ClientController.deleteClientById);
 
 export default router;
